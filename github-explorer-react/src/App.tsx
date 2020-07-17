438c29593b324183fddde631b80bf31b72b7c55e
Ermogenes Palacio <ermogenes@gmail.com>
2020-07-17 16:07:27 -0300

    Resave to apply Prettier settings

--- github-explorer-react/src/App.tsx
+++ github-explorer-react/src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
-  return (
-    <h1>Hello Github Explorer user!</h1>
-  );
+  return <h1>Hello Github Explorer user!</h1>;
 }
 
 export default App;
