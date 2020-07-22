336f52033543c4d9f6374f49fcc701d1b7ac408c
Ermogenes Palacio <ermogenes@gmail.com>
2020-07-22 19:04:06 -0300

    Updates

---
+++ precache-manifest.ac4e24b9de164841dc739de6fa7e189d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7371521e677b1c0cc3797e74772f1c",
+    "url": "/gostack-s03/index.html"
+  },
+  {
+    "revision": "2c8fbf2fec7b01ba784f",
+    "url": "/gostack-s03/static/js/2.34a590ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gostack-s03/static/js/2.34a590ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c26cf643b51724ffd4",
+    "url": "/gostack-s03/static/js/main.ba27d362.chunk.js"
+  },
+  {
+    "revision": "2374b14ff44c5c900dad",
+    "url": "/gostack-s03/static/js/runtime-main.fdd30b2a.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/gostack-s03/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/gostack-s03/static/media/logo.04f6798f.svg"
+  }
+]);
