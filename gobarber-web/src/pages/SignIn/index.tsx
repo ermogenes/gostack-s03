3d3df47109e879197535d1debd91d800d3678e91
Ermogenes Palacio <ermogenes@gmail.com>
2020-08-07 18:13:52 -0300

    Refactor inputs and buttons to components

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -12,9 +14,14 @@ const SignIn: React.FC = () => {
         <img src={logoImg} alt="GoBarber" />
         <form>
           <h1>Faça seu logon</h1>
-          <input placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit">Entrar</button>
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
         </form>
         <a href="signup">
